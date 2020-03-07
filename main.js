5208f412e4c8ff470a77844b4ad32bc836e04892
Vince Ai <vinceai@outlook.com>
2020-03-07 10:03:22 -0800

    Front Page Change

--- main.js
+++ main.js
@@ -1,15 +1,43 @@
-
 function btnHover(obj){
     var oDot = document.getElementById('movingDot');
     oDot.style.animation = 'none';
-    startMove(oDot, {'top': obj.offsetTop, 'left': obj.offsetLeft, 'height': 40, 'width': 80}, 1);
+    startMove(oDot, {'top': obj.offsetTop, 'left': obj.offsetLeft, 'height': obj.offsetHeight, 'width': obj.offsetWidth}, 1);
 }
 
-function btnUnhover(obj){
+function btnUnhover(obj, fnEnd){
     var oDot = document.getElementById('movingDot');
     startMove(oDot, {}, 100, function () {
         startMove(oDot, {'top': 300, 'left': obj.offsetLeft, 'height': 20, 'width': 20}, 1, function () {
             oDot.style.animation = 'anim 5s infinite';
         })
     })
+}
+
+function changeStatus(obj, str, event) {
+    obj.style.display = str;
+    event.stopPropagation;
+}
+
+window.onscroll = function () {
+    var scrollTop = document.documentElement.scrollTop;
+    var body = document.body, html = document.documentElement;
+    var height = Math.max(body.scrollHeight, body.offsetHeight,
+            html.clientHeight, html.scrollHeight, html.offsetHeight );
+    document.getElementById('barMask').style.right = (1 - scrollTop/(height-document.documentElement.clientHeight)) * 100 + 'vw';
+}
+
+function displayBlock(obj, index, a){
+    var oFeature = document.getElementsByClassName('feature');
+    if(a == -1){
+        startMoveWH(obj, {'height': 80, 'top': 200}, 10, displayChange(oFeature, obj, 'none'))
+    }
+    else{
+        startMoveWH(obj, {'height':20, 'top': 200 + index * 20}, 10, displayChange(oFeature, obj, 'flex'))
+    }
+}
+
+function displayChange(oFeature, obj, str){
+    for(var i = 0; i < oFeature.length; i++){
+        if(oFeature[i].id != obj.id) oFeature[i].style.display = str;
+    }
 }
